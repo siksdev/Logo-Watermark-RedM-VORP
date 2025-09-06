3cfa7f8fc77b928c580cd623130497a44d2cdd86
Renato Fernandes <contato.siksdev@gmail.com>
2025-09-06 12:16:10 -0300

    Update fxmanifest.lua

--- logo_watermark/fxmanifest.lua
+++ logo_watermark/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'rdr3'
 author 'SIKSDEV'
-description 'This script is made by JustRoy and tested'
+description 'This script is made by SIKSDEV and tested'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 ui_page 'html/ui.html'
@@ -13,3 +13,4 @@ files {
 }
 
 client_script 'client.lua'
+
